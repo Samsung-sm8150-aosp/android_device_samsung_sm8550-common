ed76c5a0ba04ce057d172011a0b7bdec9b198f9a
Giovanni Ricca <giovanniricca@proton.me>
2024-06-16 17:00:16 +0200

    sm8550-common: Add GraphicBufferMapper::lock/unlock shim
    * Commit Ib4590b7a3b839a993f6c747c5f09a6312f7ab329 modified
      GraphicBufferMapper::lock/unlock overload. Create a shim to provide the old
      symbol for blobs that need it
    
    Change-Id: I0e90ac0b383b05904df83076d990ad9335a40664

---
+++ libshims/GraphicBufferMapper.cpp
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2024 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <sync/sync.h>
+#include <ui/GraphicBufferMapper.h>
+#include <ui/Rect.h>
+
+using android::Rect;
+using android::status_t;
+
+extern "C" {
+status_t _ZN7android19GraphicBufferMapper4lockEPK13native_handlejRKNS_4RectEPPvPiS9_(
+        void* thisptr, buffer_handle_t handle, uint32_t usage, const Rect& bounds, void** vaddr,
+        int32_t* /*outBytesPerPixel*/, int32_t* /*outBytesPerStride*/) {
+    auto* gpm = static_cast<android::GraphicBufferMapper*>(thisptr);
+    return gpm->lock(handle, usage, bounds, vaddr);
+}
+
+status_t _ZN7android19GraphicBufferMapper6unlockEPK13native_handle(void* thisptr,
+                                                                   buffer_handle_t handle) {
+    android::base::unique_fd outFence;
+    auto* gpm = static_cast<android::GraphicBufferMapper*>(thisptr);
+    status_t status = gpm->unlock(handle, &outFence);
+    if (status == android::OK && outFence.get() >= 0) {
+        sync_wait(outFence.get(), -1);
+        outFence.reset();
+    }
+    return status;
+}
+}
